0045c883b637428235c34d799ed1b8ceb1bed2e7
jncraton <jncraton@gmail.com>
2025-12-05 01:27:35 -0500

    Return None for oob

--- 4.py
+++ 4.py
@@ -3,6 +3,9 @@ grid = [list(row.strip()) for row in open('4-1.txt').readlines()]
 print(grid)
 
 def get(grid, x, y):
+    if x < 0 or y < 0:
+        return None
+
     return grid[y][x]
 
 print(get(grid, -1, -1))
