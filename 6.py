08e815c22f5546eb01e0d29f637a314c8e0bc766
jncraton <jncraton@gmail.com>
2025-12-06 06:59:09 -0500

    Complete part 1

--- 6.py
+++ 6.py
@@ -1,3 +1,16 @@
-problems = list(zip(*[r.split() for r in open('6-1.txt').readlines()]))
+from math import prod
 
-print(problems)
+problems = list(zip(*[r.split() for r in open('6.txt').readlines()]))
+
+total = 0
+
+for problem in problems:
+    op = problem[-1]
+    operands = [int(p) for p in problem[:-1]]
+
+    if op == "+":
+        total += sum(operands)
+    if op == "*":
+        total += prod(operands)
+
+print(total)
