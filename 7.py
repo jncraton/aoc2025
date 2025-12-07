62ff98060b70a826d1961470916430d7945ee5c7
jncraton <jncraton@gmail.com>
2025-12-07 07:25:36 -0500

    Complete part 2

--- 7.py
+++ 7.py
@@ -1,4 +1,4 @@
-rows = [r.strip() for r in open('7.txt').readlines()]
+rows = [r.strip() for r in open('7-1.txt').readlines()]
 
 beams = {rows[0].find('S')}
 rows = rows[1:]
@@ -13,4 +13,22 @@ for row in rows:
             beams.add(beam+1)
             splits += 1
 
-print(splits)
+#print(splits)
+
+# Part 2
+
+from collections import Counter
+
+rows = [r.strip() for r in open('7.txt').readlines()]
+beams = Counter([rows[0].find('S')])
+rows = rows[1:]
+
+for row in rows:
+    for beam, count in beams.copy().items():
+        if row[beam] == '^' and count > 0:
+            beams[beam] -= count
+            beams[beam-1] += count
+            beams[beam+1] += count
+
+print(sum(count for _, count in beams.items()))
+
