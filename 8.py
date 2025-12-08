43115c65766e73cd0edb1e45e791e0ca7f70daa4
jncraton <jncraton@gmail.com>
2025-12-08 11:56:11 -0500

    Remove debug printing

--- 8.py
+++ 8.py
@@ -2,58 +2,43 @@ from itertools import combinations
 from collections import Counter
 from math import prod
 
-boxes = [tuple([int(x) for x in line.strip().split(',')]) for line in open('8.txt').readlines()]
+boxes = [
+    tuple([int(x) for x in line.strip().split(",")])
+    for line in open("8.txt").readlines()
+]
 
 # Pre-compute all distances, then sort
 
 pairs = []
 
 for a, b in combinations(boxes, 2):
-    distance = sum((ai-bi)**2 for ai,bi in zip(a,b))
+    distance = sum((ai - bi) ** 2 for ai, bi in zip(a, b))
     pairs.append((distance, a, b))
 
 pairs.sort()
 
 circuits = {}
 next_circuit = 0
-trace = tuple()
 
 for distance, a, b in pairs[:1000000]:
-    if circuits.get(a) in trace or circuits.get(b) in trace:
-        print('start', distance, a, b, circuits.get(a), circuits.get(b)) 
-
     if a in circuits and b in circuits:
         # Replace circuit b id to circuit a id
         old_id = circuits[b]
         for box in list(circuits.keys()):
             if circuits[box] == old_id:
                 circuits[box] = circuits[a]
-        if circuits.get(a) in trace or circuits.get(b) in trace:
-            print('merged', distance, a, b, circuits[a], circuits[b]) 
     elif a in circuits:
         circuits[b] = circuits[a]
-        if circuits.get(a) in trace or circuits.get(b) in trace:
-            print('set b', distance, a, b, circuits[a], circuits[b]) 
     elif b in circuits:
         circuits[a] = circuits[b]
-        if circuits.get(a) in trace or circuits.get(b) in trace:
-            print('set a', distance, a, b, circuits[a], circuits[b]) 
     else:
         circuits[a] = next_circuit
         circuits[b] = next_circuit
         next_circuit += 1
-        if circuits.get(a) in trace or circuits.get(b) in trace:
-            print('new', distance, a, b, circuits[a], circuits[b]) 
 
-    if circuits[a] in trace or circuits[b] in trace:
-        print('end', distance, a, b, circuits[a], circuits[b])
-
-    counts = Counter(circuits.values())
-
-    if counts.most_common(1)[0][1] == len(boxes):
+    if Counter(circuits.values()).most_common(1)[0][1] == len(boxes):
         print(a[0] * b[0])
         break
 
-
-#total = prod(count for _, count in counts.most_common(3))
-#print(total)
+# total = prod(count for _, count in counts.most_common(3))
+# print(total)
