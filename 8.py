c5f00c9ece96e0a30b3e8fc4b8acf1390cb84c0c
jncraton <jncraton@gmail.com>
2025-12-08 10:43:30 -0500

    Precompute pairs

--- 8.py
+++ 8.py
@@ -1,3 +1,15 @@
-positions = [[int(x) for x in line.strip().split(',')] for line in open('8-1.txt').readlines()]
+from itertools import combinations
 
-print(positions)
+boxes = [tuple([int(x) for x in line.strip().split(',')]) for line in open('8-1.txt').readlines()]
+
+# Pre-compute all distances, then sort
+
+pairs = []
+
+for a, b in combinations(boxes, 2):
+    distance = sum(ai*bi for ai,bi in zip(a,b))
+    pairs.append((distance, a, b))
+
+pairs.sort()
+
+print(pairs)
