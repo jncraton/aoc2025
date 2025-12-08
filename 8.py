3040833431b68ee78dcf0e4be5e6671ee9d5405c
jncraton <jncraton@gmail.com>
2025-12-08 11:49:09 -0500

    Complete part 1

--- 8.py
+++ 8.py
@@ -1,6 +1,8 @@
 from itertools import combinations
+from collections import Counter
+from math import prod
 
-boxes = [tuple([int(x) for x in line.strip().split(',')]) for line in open('8-1.txt').readlines()]
+boxes = [tuple([int(x) for x in line.strip().split(',')]) for line in open('8.txt').readlines()]
 
 # Pre-compute all distances, then sort
 
@@ -12,4 +14,42 @@ for a, b in combinations(boxes, 2):
 
 pairs.sort()
 
-print(pairs)
+circuits = {}
+next_circuit = 0
+trace = tuple()
+
+for distance, a, b in pairs[:1000]:
+    if circuits.get(a) in trace or circuits.get(b) in trace:
+        print('start', distance, a, b, circuits.get(a), circuits.get(b)) 
+
+    if a in circuits and b in circuits:
+        # Replace circuit b id to circuit a id
+        old_id = circuits[b]
+        for box in list(circuits.keys()):
+            if circuits[box] == old_id:
+                circuits[box] = circuits[a]
+        if circuits.get(a) in trace or circuits.get(b) in trace:
+            print('merged', distance, a, b, circuits[a], circuits[b]) 
+    elif a in circuits:
+        circuits[b] = circuits[a]
+        if circuits.get(a) in trace or circuits.get(b) in trace:
+            print('set b', distance, a, b, circuits[a], circuits[b]) 
+    elif b in circuits:
+        circuits[a] = circuits[b]
+        if circuits.get(a) in trace or circuits.get(b) in trace:
+            print('set a', distance, a, b, circuits[a], circuits[b]) 
+    else:
+        circuits[a] = next_circuit
+        circuits[b] = next_circuit
+        next_circuit += 1
+        if circuits.get(a) in trace or circuits.get(b) in trace:
+            print('new', distance, a, b, circuits[a], circuits[b]) 
+
+    if circuits[a] in trace or circuits[b] in trace:
+        print('end', distance, a, b, circuits[a], circuits[b]) 
+
+counts = Counter(circuits.values())
+
+total = prod(count for _, count in counts.most_common(3))
+
+print(total)
