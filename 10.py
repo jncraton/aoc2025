0824ec4af77bddebb50eb8cbd2811c2a72f2e47e
jncraton <jncraton@gmail.com>
2025-12-10 12:07:01 -0500

    Attempt improved heuristic

--- 10.py
+++ 10.py
@@ -3,7 +3,7 @@ from queue import PriorityQueue
 
 machines = []
 
-for line in open('10.txt').readlines():
+for line in open('10-1.txt').readlines():
     machines.append({
         "goal": tuple(b == '#' for b in re.search(r'\[(.*?)\]', line)[1]),
         "edges": [tuple(int(n) for n in e.split(',')) for e in re.findall(r'\((.*?)\)', line)],
@@ -48,37 +48,47 @@ print(total)
 # This is BFS over the button combos
 
 total = 0
+iters = 0
 
-for machine in machines:
+for i, machine in enumerate(machines):
+    print(f"{i}/{len(machines)}: {machine['joltage_goal']} {machine['edges']}")
     frontier = PriorityQueue()
     start = tuple([0] * len(machine['joltage_goal']))
     visited = set()
     visited.add(start)
     for edge in machine['edges']:
-        frontier.put((sum(machine['joltage_goal'])//len(machine['joltage_goal']), 0, start, edge))
+        frontier.put((0, 0, start, edge))
 
+    best_press = max(len(e) for e in machine['edges'])
+
+    last_heuristic = 0
     while True:
+        iters+=1
         heuristic, cost, state, edge = frontier.get()
-
-        cost += 1
-
-        state = tuple(j+1 if i in edge else j for i, j in enumerate(state))
-
-        if state in visited:
-            continue
-
-        heuristic = cost + sum(goal - cur for cur, goal in zip(state, machine['joltage_goal'])) //len(machine['joltage_goal'])
-
-        if any(cur > goal for cur, goal in zip(state, machine['joltage_goal'])):
-            continue
-
-        visited.add(state)
 
         if state == machine['joltage_goal']:
             total += cost
             break
 
-        for edge in machine['edges']:
-            frontier.put((heuristic, cost, state, edge))
+        cost += 1
 
+        if any(cur > goal for cur, goal in zip(state, machine['joltage_goal'])):
+            continue
+
+        distance = sum(goal - cur for cur, goal in zip(state, machine['joltage_goal']))
+
+        heuristic = cost + distance / best_press
+
+        if heuristic > last_heuristic:
+            print(i, heuristic, distance, cost, state, edge)
+            last_heuristic = heuristic
+
+        for edge in machine['edges']:
+            next_state = tuple(j+1 if i in edge else j for i, j in enumerate(state))
+
+            if next_state not in visited:
+                visited.add(next_state)
+                frontier.put((heuristic, cost, next_state, edge))
+
+print("Total iterations:", iters)
 print(total)
