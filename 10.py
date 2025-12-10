68e604454a8feb9fda1c047fea5fcbca7b548932
jncraton <jncraton@gmail.com>
2025-12-10 09:31:56 -0500

    Theoretically functional p2, needs state pruning

--- 10.py
+++ 10.py
@@ -7,7 +7,7 @@ for line in open('10.txt').readlines():
     machines.append({
         "goal": tuple(b == '#' for b in re.search(r'\[(.*?)\]', line)[1]),
         "edges": [tuple(int(n) for n in e.split(',')) for e in re.findall(r'\((.*?)\)', line)],
-        "weights": tuple(int(n) for n in re.search(r'\{(.*?)\}', line)[1].split(',')),
+        "joltage_goal": tuple(int(n) for n in re.search(r'\{(.*?)\}', line)[1].split(',')),
     })
 
 # This is BFS over the button combos
@@ -42,3 +42,38 @@ for machine in machines:
             frontier.put((cost, state, edge))
 
 print(total)      
+
+# Part 2
+
+# This is BFS over the button combos
+
+total = 0
+
+for machine in machines:
+    visited = set()
+    frontier = PriorityQueue()
+    start = tuple([0] * len(machine['joltage_goal']))
+    visited.add(start)
+    for edge in machine['edges']:
+        frontier.put((0, start, edge))
+
+    while True:
+        cost, state, edge = frontier.get()
+
+        cost += 1
+
+        state = tuple(j+1 if i in edge else j for i, j in enumerate(state))
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+
+        if state == machine['joltage_goal']:
+            total += cost
+            break
+
+        for edge in machine['edges']:
+            frontier.put((cost, state, edge))
+
+print(total)
