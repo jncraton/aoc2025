c4966a3408bef4243bd5b18877ad54d57b1729f2
jncraton <jncraton@gmail.com>
2025-12-10 08:50:58 -0500

    Fix warning

--- 10.py
+++ 10.py
@@ -4,9 +4,9 @@ machines = []
 
 for line in open('10-1.txt').readlines():
     machines.append({
-        "goal": tuple(b == '#' for b in re.search('\[(.*?)\]', line)[1]),
-        "edges": [tuple(int(n) for n in e.split(',')) for e in re.findall('\((.*?)\)', line)],
-        "weights": tuple(int(n) for n in re.search('\{(.*?)\}', line)[1].split(',')),
+        "goal": tuple(b == '#' for b in re.search(r'\[(.*?)\]', line)[1]),
+        "edges": [tuple(int(n) for n in e.split(',')) for e in re.findall(r'\((.*?)\)', line)],
+        "weights": tuple(int(n) for n in re.search(r'\{(.*?)\}', line)[1].split(',')),
     })
 
 # This is BFS over the button combos
